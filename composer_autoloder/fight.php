77997b932dd00e4412e104eefdba766d44634821
kosankuz <konstantinkuzmenko@icloud.com>
2019-05-11 16:23:17 +0100

    autoload

---
+++ composer_autoloder/fight.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: konstantinkuzmenko
+ * Date: 11/05/2019
+ * Time: 16:18
+ */
+
+
+use Acme\Animal;
+
+$animal = new Animal();
+
+echo $animal->roar();
---
+++ composer_autoloder/src/animal.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: konstantinkuzmenko
+ * Date: 11/05/2019
+ * Time: 15:58
+ */
+
+namespace Acme;
+
+class Animal {
+    public function roar() {
+        return "Roar made some happiness";
+    }
+}
