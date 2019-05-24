f5e9f4e6562c2cebd1edcc4f95942f5adaf62e39
kosankuz <konstantinkuzmenko@icloud.com>
2019-05-24 22:10:21 +0100

    .

--- MAMP/inputPHP/second.php
+++ MAMP/inputPHP/second.php
@@ -5,8 +5,13 @@
  * Date: 24/05/2019
  * Time: 19:19
  */
+
+$varToConsole = 'console ept';
+
+
 if  (isset($_POST)){
    echo 'This value is taken fro PHP file ';
+   echo "<script> console.log('.$varToConsole.') </scirpt>";
    return;
 } else {
     echo 'nopost';
