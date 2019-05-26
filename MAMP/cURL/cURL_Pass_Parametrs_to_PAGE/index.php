cf75aacbf2674d9bff3cedea7070bc894e5a723f
kosankuz <konstantinkuzmenko@icloud.com>
2019-05-26 01:37:26 +0100

    added error check

--- MAMP/cURL/cURL_Pass_Parametrs_to_PAGE/index.php
+++ MAMP/cURL/cURL_Pass_Parametrs_to_PAGE/index.php
@@ -6,13 +6,21 @@
  * Time: 01:00
  */
 
-$data=array("a"=>"6","b"=>"3");
-$url = "http://example.com/testCURL.php";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-$returned = curl_exec($ch);
-curl_close ($ch);
-echo $returned;
+
+
+try {
+    $data=array("a"=>"6","b"=>"3");
+    $url = "http://example.com/testCURL.php";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    $returned = curl_exec($ch);
+    curl_close ($ch);
+    echo $returned;
+
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
--- MAMP/cURL/cURL_pageCOntent/index.php
+++ MAMP/cURL/cURL_pageCOntent/index.php
@@ -6,10 +6,15 @@
  * Time: 00:55
  */
 
-$url = "http://www.example.com/";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);//для возврата результата в виде строки, вместо прямого вывода в браузер
-$returned = curl_exec($ch);
-curl_close ($ch);
-echo $returned;
+try {
+    $url = "http://www.example.com/";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//для возврата результата в виде строки, вместо прямого вывода в браузер
+    $returned = curl_exec($ch);
+    curl_close($ch);
+    echo $returned;
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
