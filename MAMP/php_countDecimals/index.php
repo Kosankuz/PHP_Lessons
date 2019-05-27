601166f4d6a51d45d97ba14af228136069eb3430
kosankuz <konstantinkuzmenko@icloud.com>
2019-05-27 01:09:11 +0100

    counting decimals

---
+++ MAMP/php_countDecimals/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+</head>
+<body>
+
+<input id="input1" type="number" value="0">
+<input id="input2" type="number" value="0">
+<input id="input3" type="number" value="0">
+<input id="input4" type="number" value="0">
+
+<script src="js/jquery.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
