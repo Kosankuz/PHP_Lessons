bc01b285c9fe5e9b9d4135d40683a08de5a6eb88
kosankuz <konstantinkuzmenko@icloud.com>
2019-05-29 23:13:55 +0100

    checking if span value is changed

---
+++ MAMP/htmlChecker/js/master.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+ timer();
+
+});
+
+
+
+function timer() {
+    setInterval(function() {
+        var date = new Date();
+        var time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+        $('#jqueryOutput').text(time);
+        checker();
+    },1000)
+}
+
+
+function checker() {
+    var currentValue = $('#jqueryOutput').text();
+    var newValue;
+    setTimeout(function () {
+        var newValue = $('#jqueryOutput').text();
+        console.log('newValue' + newValue);
+        console.log('Old value' + currentValue);
+        if (currentValue !== newValue){
+
+            $('#checker').text(newValue);
+        } else {
+
+            $('#checker').text('current value eqals  new value');
+        }
+    },1000);
+
+
+}
