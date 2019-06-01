0b4abbad6856eea528a5bb8bbd8bc801caf39ea1
kosankuz <konstantinkuzmenko@icloud.com>
2019-06-01 22:54:24 +0100

    getting file image

---
+++ MAMP/getting_file_content/File_Loading/index.php
@@ -0,0 +1,36 @@
+<?php
+//require('php/data.php');
+$f = "fd";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script type="text/javascript" src="js/index.js"></script>
+    <link rel="stylesheet" href="css/master.css">
+
+    <title>Title</title>
+</head>
+<body>
+<div class="container">
+
+    <div class="modal" hidden>
+
+        Loading
+
+
+    </div>
+
+    <div class="window" style="text-align: center">
+        <p>
+            <textarea name="text" id="" cols="100" rows="30"></textarea>
+        </p>
+
+        <button class="">Загрузить текст</button>
+
+    </div>
+</div>
+
+</body>
+</html>
